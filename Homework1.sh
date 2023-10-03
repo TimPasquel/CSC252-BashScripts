369efceb33ffdd784f631dce26cb4a5697bf2849
TimPasquel <146892252+TimPasquel@users.noreply.github.com>
2023-10-03 18:15:36 -0400

    Create Homework1.sh

---
+++ Homework1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#Author: Tim Pasquel
+#Major: Information Technology
+#Creation Date: 2/1/2022
+#Due Date: 2/4/2022
+#Course: CSC252
+#Professor Name: Professor Earl
+#Assignment: Homework #1
+#Filename: hw1.sh
+#Purpose: Bash script that prints a quote, username, and filepath
+
+echo 1.
+echo "I don't want to die without any scars"
+
+echo 2.
+whoami
+
+echo 3.
+pwd
